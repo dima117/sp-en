ff608d0e584697c3ebe27317369e9980535a39c1
Dmitry Andriyanov <dima117a@gmail.com>
2023-03-08 23:27:32 +0300

    torpedo

--- SpaceEngineers/Program.cs
+++ SpaceEngineers/Program.cs
@@ -22,21 +22,74 @@ namespace SpaceEngineers
         #region Copy
 
         readonly TargetTracker tt;
-        readonly IMyRemoteControl remote;
+        readonly IMyTextPanel lcd;
+        readonly IMyCameraBlock cam;
         readonly IMyShipMergeBlock slot;
-        readonly IMySoundBlock sound;
 
-
+        Torpedo torpedo;
+       
         public Program()
         {
-            tt = new TargetTracker(this);
+            tt = new TargetTracker(this, "Камера");
+            lcd = GridTerminalSystem.GetBlockWithName("LCD") as IMyTextPanel;
+            slot = GridTerminalSystem.GetBlockWithName("SLOT") as IMyShipMergeBlock;
+            cam = GridTerminalSystem.GetBlockWithName("MAIN_CAM") as IMyCameraBlock;
+            cam.EnableRaycast = true;
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
         }
         public void Main(string argument, UpdateType updateSource)
         {
-            var vel = remote.GetShipVelocities().LinearVelocity;
-            sound.SelectedSound = "";
-            sound.Play();
+            switch (argument)
 
+            {
+                case "capture":
+                    var target = cam.Raycast(20000);
+                    if (!target.IsEmpty())
+                    {
+                        tt.LockOn(target.Position);
+                    }
+                    break;
+                case "reload":
+                    slot.Enabled = true;
+                    break;
+                case "prepare":
+                    torpedo = new Torpedo(this);
+                    break;
+                case "start":
+                    slot.Enabled = false;
+                    torpedo.Start();
+
+                    lcd.CustomData = "";
+                    break;
+                default:
+                    tt.Update();
+
+                    if (torpedo != null)
+                    {
+                        var info = torpedo.Update(tt.CurrentTarget);
+                        lcd.CustomData += "\n" + info;
+                    }
+                    break;
+            }
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Torpedo: {torpedo?.Id}");
+            sb.AppendLine($"------------------------");
+            sb.AppendLine($"Range: {cam.AvailableScanRange}");
+            sb.AppendLine($"Cam count: {tt.Count}\n------------------------");
+            sb.AppendLine($"Locked: ${!tt.CurrentTarget.IsEmpty()}");
+
+            if (!tt.CurrentTarget.IsEmpty())
+            {
+                sb.AppendLine($"Pos X: {tt.CurrentTarget.Position.X}");
+                sb.AppendLine($"Pos Y: {tt.CurrentTarget.Position.Y}");
+                sb.AppendLine($"Pos Z: {tt.CurrentTarget.Position.Z}");
+                sb.AppendLine($"Speed: {tt.CurrentTarget.Velocity.Length()}");
+                sb.AppendLine($"Distance: {Vector3D.Distance(cam.GetPosition(), tt.CurrentTarget.Position)}");
+            }
+
+            lcd.WriteText(sb.ToString());
         }
         public void Save()
         {
---
+++ SpaceEngineers/Torpedo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using VRageMath;
+using VRage.Game;
+using VRage.Game.GUI.TextPanel;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using Sandbox.Game.GameSystems;
+
+namespace SpaceEngineers
+{
+    public class Torpedo
+    {
+        public long Id { get; private set; }
+
+        IMyGyro tGyro;
+        IMyThrust tEngine;
+        IMyRemoteControl tRemote;
+        DirectionController tControl;
+
+        public Torpedo(MyGridProgram program, string prefix = "T_")
+        {
+            Id = DateTime.UtcNow.Ticks;
+
+            tEngine = program.GridTerminalSystem.GetBlockWithName($"{prefix}ENGINE") as IMyThrust;
+            tGyro = program.GridTerminalSystem.GetBlockWithName($"{prefix}GYRO") as IMyGyro;
+            tRemote = program.GridTerminalSystem.GetBlockWithName($"{prefix}REMOTE") as IMyRemoteControl;
+            tControl = new DirectionController(tRemote);
+        }
+
+        public void Start()
+        {
+            tEngine.ThrustOverridePercentage = 100;
+            tGyro.GyroOverride = true;
+        }
+
+        public string Update(MyDetectedEntityInfo target)
+        {
+            if (target.IsEmpty())
+            {
+                return "---";
+            }
+
+            var speed = tRemote.GetShipVelocities().LinearVelocity.Length();
+            var d = tControl.GetTargetAngle(target.Position);
+            tGyro.Pitch = Convert.ToSingle(d.Pitch);
+            tGyro.Yaw = Convert.ToSingle(d.Yaw);
+
+            var sb = new StringBuilder();
+
+            sb.Append($"Id: {Id}");
+            sb.AppendLine($"Speed: {speed:0.00}");
+            sb.AppendLine($"Target: {target.Position}");
+            sb.AppendLine($"Missile pos: {tRemote.GetPosition()}");
+            sb.AppendLine($"Missile forward: {tRemote.WorldMatrix.Forward}");
+            sb.AppendLine($"Missile up: {tRemote.WorldMatrix.Up}");
+            sb.AppendLine($"Missile left: {tRemote.WorldMatrix.Left}");
+
+            sb.AppendLine(d.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
