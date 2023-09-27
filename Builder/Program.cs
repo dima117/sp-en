5636d646a9cc1ee55638e04132ebeb7c0ef1e7c2
dima117a <dima117a@gmail.com>
2023-09-27 10:13:38 +0300

    builder draft + wc pb api

---
+++ Builder/Program.cs
@@ -0,0 +1,25 @@
+﻿using CSharpMinifier;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "C:\\Users\\dima117a\\source\\repos\\sp-en\\SpaceEngineers\\Program.cs";
+
+            var source = File.ReadAllText(path);
+
+            var minifiedSource = Minifier.Minify(source);
+
+            Console.Write(string.Join(string.Empty, minifiedSource));
+            Console.ReadKey();
+        }
+    }
+}
