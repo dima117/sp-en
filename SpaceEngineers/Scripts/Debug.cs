7546b13ea062154bf432dab2d69c833e25419aa1
Dmitry Andriyanov <dima117a@gmail.com>
2023-11-25 00:26:13 +0300

    1

--- SpaceEngineers/Scripts/Debug.cs
+++ SpaceEngineers/Scripts/Debug.cs
@@ -25,14 +25,18 @@ namespace SpaceEngineers.Scripts.Debug
         // нужен сотиентированный гироскоп и ротор с тем же направлением up
 
         private IMyMotorStator rotorAzimuth;
+        private IMyMotorStator rotorElevation;
         private IMyShipController remote;
-        private IMyGyro gyro;
+        private IMyGyro gyro1;
+        private IMyGyro gyro2;
 
         public Program()
         {
             rotorAzimuth = GridTerminalSystem.GetBlockWithName("AZIMUTH") as IMyMotorStator;
+            rotorElevation = GridTerminalSystem.GetBlockWithName("ELEVATION") as IMyMotorStator;
             remote = GridTerminalSystem.GetBlockWithName("REMOTE") as IMyShipController;
-            gyro = GridTerminalSystem.GetBlockWithName("GYRO") as IMyGyro;
+            gyro1 = GridTerminalSystem.GetBlockWithName("GYRO1") as IMyGyro;
+            gyro2 = GridTerminalSystem.GetBlockWithName("GYRO2") as IMyGyro;
 
             Runtime.UpdateFrequency = UpdateFrequency.Update1;
         }
@@ -41,15 +45,21 @@ namespace SpaceEngineers.Scripts.Debug
         {
             var input = remote.MoveIndicator;
 
-            gyro.GyroOverride = true;
-            gyro.Yaw = input.X;
+            gyro1.GyroOverride = true;
+            gyro2.GyroOverride = true;
 
+            gyro1.Yaw = gyro2.Yaw = input.X;
+            gyro1.Pitch = gyro2.Pitch = input.Z;
 
             var invertedMatrix = MatrixD.Invert(rotorAzimuth.WorldMatrix.GetOrientation());
+            //var invertedMatrix2 = MatrixD.Invert(rotorElevation.WorldMatrix.GetOrientation());
+
             var angularVelocity = remote.GetShipVelocities().AngularVelocity;
             var localAngularVelocity = Vector3D.Transform(angularVelocity, invertedMatrix);
+            //var localAngularVelocity2 = Vector3D.Transform(angularVelocity, invertedMatrix2);
 
-            rotorAzimuth.TargetVelocityRad = -Convert.ToSingle(localAngularVelocity.Dot(rotorAzimuth.WorldMatrix.Up));
+            rotorAzimuth.TargetVelocityRad = Convert.ToSingle(localAngularVelocity.Dot(rotorAzimuth.WorldMatrix.Up));
+            //rotorElevation.TargetVelocityRad = Convert.ToSingle(localAngularVelocity2.Dot(rotorElevation.WorldMatrix.Up));
         }
 
         #endregion
